1240e7dd34a21434a166277bc6c5180a6df5cf9d
Mikhail <59769710+BibaDev@users.noreply.github.com>
2021-01-15 16:03:32 +0300

    Add files via upload
    Init

---
+++ BibaDev/Scripts/Globalization/Localization.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+/*
+ * Created by BibaDev: (https://biba.dev)
+ * Support me (donation): https://biba.dev/donation
+ * Contact me: https://biba.dev/feedback
+ * GitHub: https://github.com/BibaDev/Unity.Globalization.Localization
+ */
+
+namespace BibaDev.Globalization
+{
+
+    public class Localization
+    {
+
+        public static bool Initialized { get; private set; }
+        public static string Language { get; private set; }
+        public static Localization General { get; private set; }
+
+        private readonly Dictionary<string, string> _table;
+
+        public static void Init(string language, string generalPath = "Localization/General")
+        {
+
+            Language = language;
+            Initialized = true;
+
+            General = new Localization(generalPath);
+            
+        }
+        
+        public Localization(string path)
+        {
+            
+            if (!Initialized)
+                throw new NullReferenceException("Localization not initialized! Call Localization.Init");
+
+            var asset = Resources.Load<TextAsset>(Path.Combine(path, Language));
+            
+            if (!asset)
+                throw new FileNotFoundException($"Asset '{Path.Combine(path, Language)}' not found!");
+
+            _table = JsonConvert.DeserializeObject<Dictionary<string, string>>(asset.text);
+
+        }
+
+        public string Get(in string key) 
+            => _table.TryGetValue(key, out var value) ? value : $"{{{key}}}" ;
+        
+    }
+    
+}
---
+++ BibaDev/Scripts/Globalization/TextLocalizer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Created by BibaDev: (https://biba.dev)
+ * Support me (donation): https://biba.dev/donation
+ * Contact me: https://biba.dev/feedback
+ * GitHub: https://github.com/BibaDev/Unity.Globalization.Localization
+ */
+
+namespace BibaDev.Globalization
+{
+    
+    [AddComponentMenu("BibaDev/Globalization/TextLocalizer")]
+    public class TextLocalizer : MonoBehaviour
+    {
+
+        [SerializeField] private string _key;
+        [SerializeField] private Text _component;
+
+        private void Start() => _component.text = Localization.General.Get(in _key);
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+
+            if (_component == null)
+                _component = GetComponent<Text>();
+            
+        }
+#endif
+        
+    }
+
+}
